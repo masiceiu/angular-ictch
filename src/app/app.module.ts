cf5f55b4cb18404e97a321f937a7d7cfa01ea7b5
masiceiu <masiceiu@gmail.com>
2020-02-02 11:14:47 +0600

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import {AppRoutingModule} from './app-routing.module';
+import {AuthService} from './../services/auth.service';
+import {AuthGuard} from './../services/guard.service';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
