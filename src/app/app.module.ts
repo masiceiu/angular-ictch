b5402303c87608f20cf644a40d40f1f9cc1c25d1
masiceiu <masiceiu@gmail.com>
2021-01-12 16:07:36 +0600

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { RouteDirective } from './route.directive';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, RouteDirective ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
