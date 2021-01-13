fb3ca5828e3d0e0b27ab2df9daf20b2ec59802c3
masiceiu <masiceiu@gmail.com>
2021-01-13 09:20:02 +0600

    1 file modified

--- src/services/guard.service.ts
+++ src/services/guard.service.ts
@@ -1,30 +1,31 @@
 import {CanActivate,ActivatedRouteSnapshot,RouterStateSnapshot,Router,CanActivateChild} from '@angular/router';
-import {Observable} from 'rxjs/Observable';
 import {AuthService} from './auth.service';
 import {Injectable} from '@angular/core';
 
 @Injectable()
-export class AuthGuard implements CanActivate,CanActivateChild{
+export class AuthGuard implements CanActivate, CanActivateChild{
 
   constructor(private authService:AuthService, private router:Router){
 
   }
 
-canActivate(route:ActivatedRouteSnapshot,state:RouterStateSnapshot): Observable<boolean>| Promise<boolean> | boolean{
- return  this.authService.isAuthenticated()
-  .then((authenticated:boolean)=>{
-    if(authenticated){
-      return true;
-    }
-    else{
-      this.router.navigate(['/']);
-    }
-  });
-}
+  canActivate(route:ActivatedRouteSnapshot,state:RouterStateSnapshot): Promise<boolean> | boolean
+  {
+  return  this.authService.isAuthenticated()
+    .then((authenticated:boolean)=>{
+      if(authenticated){
+        return true;
+      }
+      else{
+        this.router.navigate(['/']);
+      }
+    });
+  }
 
-canActivateChild(route:ActivatedRouteSnapshot, state:RouterStateSnapshot): Observable<boolean>| Promise<boolean> | boolean{
-  return this.canActivate(route,state);
-};
+  canActivateChild(route:ActivatedRouteSnapshot, state:RouterStateSnapshot): Promise<boolean> | boolean
+  {
+    return this.canActivate(route,state);
+  }         ;
 }
 
 // CanActivate is an interface provided by angular router
