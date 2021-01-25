3aa9f59e70921bbfd6a1f41caad00441b7bd498a
masiceiu <masiceiu@gmail.com>
2021-01-25 14:22:30 +0600

    1 file modified

--- src/app/rnd/rnd.module.ts
+++ src/app/rnd/rnd.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common'
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { RndComponent } from './rnd/rnd.component';;
 import { CssLearn3Component } from './rnd/css-learn3/css-learn3.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule, BrowserModule
   ],
   declarations: [
     RndComponent,
