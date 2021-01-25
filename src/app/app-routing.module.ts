ba279bebf5d28000fe42fda5796ec3a912da8068
masiceiu <masiceiu@gmail.com>
2021-01-25 14:35:55 +0600

    2 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,14 @@ import { LandingComponent } from './pages/landing/landing.component';
 import { AboutComponent } from './pages/about/about.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { RndComponent } from './rnd/rnd.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent  },
   { path: 'landing', component: LandingComponent},
   { path: 'profile', component: ProfileComponent},
   { path: 'about', component: AboutComponent  },
+  { path: 'rnd', component: RndComponent  },
   { path: 'district', component: DistrictComponent  },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent  }
