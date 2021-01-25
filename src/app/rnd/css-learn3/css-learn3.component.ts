3ef563cf85a2faf79b39be58cc9868be8ff213a8
masiceiu <masiceiu@gmail.com>
2021-01-25 15:30:14 +0600

    5 files modified

--- src/app/rnd/css-learn3/css-learn3.component.ts
+++ src/app/rnd/css-learn3/css-learn3.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-css-learn3',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CssLearn3Component implements OnInit {
 
+  @Input() rnd = 'rnd1';
   constructor() { }
 
   ngOnInit() {
--- src/app/rnd/rnd.component.ts
+++ src/app/rnd/rnd.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-rnd',
@@ -11,6 +11,7 @@ export class RndComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
