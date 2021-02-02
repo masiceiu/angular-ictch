f061b97a2cd7f893a01b4f82232cb4cb7aaee3d1
masiceiu <masiceiu@gmail.com>
2021-02-02 16:24:35 +0600

    1 file added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@ export const environment = {
   url: {
     getStudentList: '../json/get-student-list.json',
     getTeacherList: '../json/get-teacher-list.json',
-    getDistrictList: '../json/get-district-list.json'
+    getDistrictList: '/asse/json/get-district-list.json'
   }
 };
