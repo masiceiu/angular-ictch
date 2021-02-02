7a7fefbacd65b94f58441fdfc1e9ce16b7f5c022
Abdus.Sobhan <masiceiu@gmail.com>
2021-02-02 15:40:46 +0600

    Commit of image crop & json read

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,17 +7,23 @@ import { AboutComponent } from './pages/about/about.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { RndComponent } from './rnd/rnd.component';
+import { ReadLocalJsonComponent } from './rnd/read-local-json/read-local-json.component';
+import { ImageCropperTestComponent } from './rnd/image-cropper-test/image-cropper-test.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent  },
   { path: 'landing', component: LandingComponent},
-  { path: 'profile', component: ProfileComponent},
   { path: 'about', component: AboutComponent  },
-  { path: 'rnd', component: RndComponent  },
+  { path: 'profile', component: ProfileComponent},
   { path: 'district', component: DistrictComponent  },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  //{ path: 'json' },
-  { path: '**', component: PageNotFoundComponent  }
+  
+  { path: 'rnd', component: RndComponent  },
+  { path: 'profile', component: ProfileComponent},
+  { path: 'local-json', component: ReadLocalJsonComponent},
+  { path: 'cropper-test', component: ImageCropperTestComponent},
+  
+  { path: '**', component: PageNotFoundComponent  },
 ];
 
 @NgModule({
--- src/app/rnd/rnd.module.ts
+++ src/app/rnd/rnd.module.ts
@@ -1,17 +1,44 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common'
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { RndComponent } from './rnd.component';;
 import { CssLearn3Component } from './css-learn3/css-learn3.component';
 
+import { ImageCropperModule } from './image-cropper/image-cropper.module';
+import { ImageCropperTestComponent } from './image-cropper-test/image-cropper-test.component';
+import { 
+  ConfigService, 
+  SettingsService 
+} from './index';
+import { ReadLocalJsonComponent } from './read-local-json/read-local-json.component';
+
 @NgModule({
   imports: [
-    CommonModule, BrowserModule
+    CommonModule, 
+    FormsModule,
+    BrowserModule,
+    HttpClientModule,
+    ImageCropperModule
   ],
   declarations: [
     RndComponent,
-    CssLearn3Component,
-    ]
+    CssLearn3Component, 
+    ReadLocalJsonComponent, 
+	  ImageCropperTestComponent
+
+  ],
+  exports: [
+    CommonModule,
+    FormsModule,
+    BrowserModule,
+    HttpClientModule,
+    ImageCropperModule,
+    ReadLocalJsonComponent, 
+    ImageCropperTestComponent
+  ],
+  providers: [ConfigService, SettingsService],
+
 })
 export class RndModule { }
