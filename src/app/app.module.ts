43cab8a379cde75327930c1a3d3cae7b07a8f8cf
masiceiu <masiceiu@gmail.com>
2021-02-06 10:45:43 +0600

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { FormsModule } from '@angular/forms';
 import {Routes,RouterModule} from '@angular/router';
 
 import {AppRoutingModule} from './app.routing.module';
-import {AuthService} from './../services/auth.service';
-import {AuthGuard} from './../services/guard.service';
+import {AuthService} from './common/services/auth.service';
+import {AuthGuard} from './common/services/guard.service';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './../home/home.component';
-import { ToolBarComponent } from './../home/tool-bar/tool-bar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ToolBarComponent } from './shared/component/tool-bar/tool-bar.component';
 
 @NgModule({
   imports:      
