284ba94e4f00f77570e6842dfbab1eab4a019452
masiceiu <masiceiu@gmail.com>
2021-04-04 12:29:57 +0600

    2 files modified, 1 added

--- src/app/rnd/aysha-design/my-bootstrap4/my-bootstrap4.component.ts
+++ src/app/rnd/aysha-design/my-bootstrap4/my-bootstrap4.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-my-bootstrap4',
+  selector: 'app-aysha-bootstrap4',
   templateUrl: './my-bootstrap4.component.html',
   styleUrls: ['./my-bootstrap4.component.css']
 })
